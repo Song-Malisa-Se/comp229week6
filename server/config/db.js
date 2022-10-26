9633f8a5f8390d822c777eb1c0579cde493fc6e9
Song Malisa Se <sesongmaliisa@gmail.com>
2022-10-26 05:53:15 -0400

    after week 6 materials

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 module.exports =
 {
     //"URI":"mongodb://localhost/book_store"
-    "URI": "mongodb+srv://malisa:Gdg1ldwoE2NRMfKh@mongodbserver.ijfe4gl.mongodb.net/book_store?retryWrites=true&w=majority"
-    
+    "URI": "mongodb+srv://malisa:Gdg1ldwoE2NRMfKh@mongodbserver.ijfe4gl.mongodb.net/book_store?retryWrites=true&w=majority",
+    "Secret": 'SomeSecret'
 }
--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,6 +3,10 @@ let router = express.Router();
 let mongoose = require('mongoose');
 let passport=require('passport');
 
+// enable jwt
+let jwt = require('jsonwebtoken');
+let DB = require('../config/db');
+
 // create the user Model instance
 let userModel = require('../models/user');
 let User = userModel.User; //alias
@@ -61,6 +65,25 @@ module.exports.processLoginPage = (req, res, next) => {
             {
                 return next(err);
             }
+            const payload =
+            {
+                id: user._id,
+                displayName: user.displayName,
+                username: user.username,
+                email: user.email
+            }
+            const authToken = jwt.sign(payload, DB.Secret, {
+                expiresIn: 604800 // 1 week
+            });
+/* - TODO Getting raedy to convert to API
+            res.json({success: true, msg: 'User Logged in Successfully!', user: {
+                id: user._id,
+                displayName: user.displayName,
+                username: user.username,
+                email: user.email
+            }, token: authToken}); 
+            */
+
             return res.redirect('/book-list');
         });
         
@@ -118,6 +141,11 @@ module.exports.processRegisterPage= (req, res, next) =>{
             // if no error exists, the registration is successful
 
             // redirect the user and authenticate them
+
+            /* TODO Getting ready to convert to API
+
+            res.json({success: true, msg: 'User Registered Successfully!'});
+            */
 
             return passport.authenticate('local')(req, res, () => {
                 res.redirect('/book-list')
