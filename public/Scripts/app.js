965492558044f3fc95c66a6c4a38e995b8db5e62
Song Malisa Se <sesongmaliisa@gmail.com>
2022-10-26 03:44:21 -0400

    first commit week6

---
+++ public/Scripts/app.js
@@ -0,0 +1,28 @@
+// IIFE -- Immediately Invoked Function
+
+
+
+// assignment1 Song Malisa Se, 301233051, 05/10/2022
+(function(){
+
+    function Start()
+    {
+        console.log("App Started...");
+
+        let deleteButtons = document.querySelectorAll('.btn-danger')
+
+        for(button of deleteButtons)
+        {
+            button.addEventListener('click', (event)=> {
+                if(!confirm("Are you sure you want to delete?"))
+                {
+                    event.preventDefault();
+                    window.location.assign('/book-list');
+                }
+            })
+        }
+    }
+
+    window.addEventListener("load", Start);
+
+})();
---
+++ server/routes/book.js
@@ -0,0 +1,37 @@
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+
+let passport = require('passport');
+
+let bookController = require('../controllers/book');
+
+// helper function for guard purposes
+function requireAuth(req, res, next)
+{
+    // check if the user is logged in
+    if(!req.isAuthenticated())
+    {
+        return res.redirect('/login');
+    }
+    next();
+}
+
+
+/* Get Route for the Book List page - READ Operation */
+router.get('/', bookController.displayBookList);
+
+/* Get Route for displaying the Add page - CREATE Operation */
+router.get('/add', requireAuth, bookController.displayAddPage);
+
+/* Post Route for processing the Add page - CREATE Operation */
+router.post('/add', requireAuth, bookController.processAddPage);
+
+/* Get Route for displaying the Edit page - UPDATE Operation */
+router.get('/edit/:id',  requireAuth, bookController.displayEditPage); 
+/* Post Route for processing the Edit page - UPDATE Operation */
+router.post('/edit/:id',  requireAuth, bookController.processEditPage); 
+/* Get Route to perform Book Deletion - DELETE Operation */
+router.get('/delete/:id', requireAuth, bookController.performDelete); 
+
+module.exports = router;
