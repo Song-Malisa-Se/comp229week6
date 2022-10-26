fd238cdb7cb8988324be50d797ffb0d515b0377a
Song Malisa Se <sesongmaliisa@gmail.com>
2022-10-26 05:03:50 -0400

    updated db URI to work with MongoDN Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,6 @@
 module.exports =
 {
-    "URI":"mongodb://localhost/book_store"
+    //"URI":"mongodb://localhost/book_store"
+    "URI": "mongodb+srv://malisa:Gdg1ldwoE2NRMfKh@mongodbserver.ijfe4gl.mongodb.net/book_store?retryWrites=true&w=majority"
+    
 }
